2702f68e0eeb63d27e6f489bb321feefb02599f7
aaronlisman <donkey00>
2017-06-22 17:23:09 -0400

    cleanup: get rid of getClinicalInformationData and tests

--- src/pages/patientView/clinicalInformation/lib/convertSamplesData.spec.ts
+++ src/pages/patientView/clinicalInformation/lib/convertSamplesData.spec.ts
@@ -1,9 +1,9 @@
 import mockClinicalData from "../../../../shared/api/mock/Clinical_data_study_ucec_tcga_pub.json";
 import { ClinicalData } from "../../../../shared/api/generated/CBioPortalAPI";
 import { default as convertSampleData, IConvertedSamplesData } from "./convertSamplesData";
-import { groupByEntityId } from '../getClinicalInformationData';
 import { ClinicalDataBySampleId } from "../../../../shared/api/api-types-extended";
 import {assert} from "chai";
+import {groupByEntityId} from "../PatientViewPageStore";
 
 describe('convertSamplesData', () => {
     it('api data is properly transformed into table data', () => {
